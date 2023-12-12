ab1a351ff5080520f0d9dc2d5a503ded1b16a8c3
Lefski <93370391+Lefski@users.noreply.github.com>
2023-12-12 04:32:30 +0300

    feat: database created

---
+++ src/main/java/ru/mgroup/carbuilder/model/BodiesType.java
@@ -0,0 +1,20 @@
+package ru.mgroup.carbuilder.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "bodies_type")
+public class BodiesType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "type_name")
+    private String typeName;
+}
